13bcec08da6be1975152c6c5267af2f9e8993583
Sidhant Sharma <sidhant1541@gmail.com>
2022-11-05 21:12:41 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+    'X-RapidAPI-Key': '53ec28c02cmsh47ff61d08f7ae36p1568adjsnfa9830239b8d',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+}
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
+export const WEATHER_API_KEY = "fad44fb39efb436f8ad2da78bffd9584";
